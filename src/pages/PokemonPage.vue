529fc8a135d380786c9dde54f4ddc9919b4fb57f
Fernando Juarez <sirferjuar@gmail.com>
2023-08-17 19:47:03 -0600

    first commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import PokemonPicture from '@/components/PokemonPicture.vue'
+    
+export default {
+        components: { PokemonOptions, PokemonPicture }
+    }
+</script>
+
+
+<template>
+    <h1>¿Quién es este pokemon?</h1>
+
+    <PokemonPicture :pokemonId="25" :showPokemon="false" />
+    <PokemonOptions />
+</template>
+
+
+<style>
+
+</style>
